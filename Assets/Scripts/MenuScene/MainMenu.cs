b9e4381648cc65354016547b4c6e5f4ade8e6550
Siroj <s.iskhakov@kino-mo.com>
2020-04-17 13:25:57 +0300

    changed menu design. edited level 1

--- Assets/Scripts/MenuScene/MainMenu.cs
+++ Assets/Scripts/MenuScene/MainMenu.cs
@@ -215,5 +215,13 @@ public class MainMenu : MonoBehaviour
 		}
 	}
 
+    public void OurSite()
+    {
+		Application.OpenURL("https://unity.com");
+	}
 
+	public void RateUs()
+	{
+		Application.OpenURL("https://play.google.com/store/apps/details?id=by.ISGames.SpacyDracy");
+	}
 }
